49ffc1d0e9b1d409792ca04333847c49d0510e09
Cycleurope <54021538+Cycleurope@users.noreply.github.com>
2020-06-19 16:00:36 +0200

    Update ReportController.php

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Report;
+use App\IMPORTS\ReportsImport;
 use App\Exports\ReportsExport;
 use Maatwebsite\Excel\Facades\Excel;
 
