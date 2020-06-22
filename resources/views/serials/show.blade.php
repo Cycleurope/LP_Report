7353ed3442fcbed9516c08e20376f00e8087501d
Cycleurope <54021538+Cycleurope@users.noreply.github.com>
2020-06-22 16:38:19 +0200

    Update show.blade.php

--- resources/views/serials/show.blade.php
+++ resources/views/serials/show.blade.php
@@ -3,7 +3,7 @@
 <div class="container-fluid px-5 py-5">
     <div class="row">
         <div class="col-12">
-            <h1>Serials</h1>
+            <h1>{( $serial->code )}</h1>
         </div>
     </div>
     <div class="row">
