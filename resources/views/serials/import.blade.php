0da7d4bc25a1a042f614a970a707874b82e825ec
Cycleurope <54021538+Cycleurope@users.noreply.github.com>
2020-06-23 11:26:50 +0200

    Commit

--- resources/views/serials/import.blade.php
+++ resources/views/serials/import.blade.php
@@ -3,7 +3,7 @@
 <div class="container-fluid px-5 py-5">
     <div class="row">
         <div class="col-12">
-            <h1>Serials Import Form</h1>
+            <h1>Importer des numéros de série</h1>
         </div>
     </div>
     <div class="row">
@@ -11,7 +11,7 @@
             <form action="{{ route('serials.import.post') }}" enctype="multipart/form-data" method="POST">
                 @csrf
                 <div class="form-group">
-                    <label for="file">File</label>
+                    <label for="file">Fichier</label>
                     <input type="file" name="file" id="file" class="form-control">
                 </div>
                 <button type="submit" class="btn btn-primary btn-block">Importer</button>
