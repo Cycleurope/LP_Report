001977bd5ee82a7e5caa0d6bc4645ee9c972e2cc
Cycleurope <54021538+Cycleurope@users.noreply.github.com>
2020-06-23 11:28:28 +0200

    Update import.blade.php

--- resources/views/users/import.blade.php
+++ resources/views/users/import.blade.php
@@ -1,9 +1,9 @@
 @extends('layouts.app')
 @section('content')
-<div class="container-flui p-5">
+<div class="container p-5">
     <div class="row">
         <div class="col-12">
-            <h1>Import Users</h1>
+            <h1>Importer des comptes Utilisateurs</h1>
         </div>
     </div>
     <div class="row">
@@ -11,7 +11,7 @@
             <form action="{{ route('users.import.post') }}" enctype="multipart/form-data" method="POST">
                 @csrf
                 <div class="form-group">
-                    <label for="file">File</label>
+                    <label for="file">Fichier</label>
                     <input type="file" name="file" id="file" class="form-control">
                 </div>
                 <button type="submit" class="btn btn-primary btn-block">Importer</button>
