df04447c1921ffc00d304cf7d817daa22598cd6a
Cycleurope <web@cycleurope.fr>
2020-06-23 17:45:15 +0200

    Commit

--- public/js/app.js
+++ public/js/app.js
@@ -65860,8 +65860,8 @@ if (document.getElementById('example')) {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/rlomvin/webapps/LP_Report/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/rlomvin/webapps/LP_Report/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/vincentlombard/Webapps/Cycleurope/LP_Report/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Users/vincentlombard/Webapps/Cycleurope/LP_Report/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
