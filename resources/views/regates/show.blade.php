c55cbf6475a5c3b5a86931b6c5321996ff3e81d8
Cycleurope <54021538+Cycleurope@users.noreply.github.com>
2020-06-23 11:04:23 +0200

    Update show.blade.php

--- resources/views/regates/show.blade.php
+++ resources/views/regates/show.blade.php
@@ -3,7 +3,7 @@
 <div class="container-fluid p-5">
     <div class="row">
         <div class="col-12">
-            <h1>{{ $regate->code }} - {{ $regate->name }}</h1>
+            <h1>{{ $regate->code }} - {{ $regate->name }}</h>
         </div>
     </div>
     <div class="row">
@@ -16,7 +16,7 @@
             <div class="card border-0">
                 <div class="card-body">
                     @if(count($regate->reports))
-                    <h2>Rapports</h2>
+                    <h2>Rapports associés</h2>
                     <table class="table table-sm">
                         <thead>
                             <tr>
@@ -24,8 +24,8 @@
                                 <th>Code Article</th>
                                 <th>Audit / Contrôle</th>
                                 <th>Date Audit / Contrôle</th>
-                                <th>Fissure</th>
-                                <th>Longueur Fissure</th>
+                                <th>Micro-fissure</th>
+                                <th>Longueur (en mm))</th>
                                 <th>Observations</th>
                             </tr>
                         </thead>
@@ -34,7 +34,7 @@
                             <tr>
                                 <td>{{ $report->serial->code }}</td>
                                 <td>{{ $report->serial->product->code }}</td>
-                                <td>{{ $report->type }}</td>
+                                <td>{!! $report->friendlyType !!}</td>
                                 <td>{{ $report->report_date }}</td>
                                 <td>{!! $report->crackStatus() !!}</td>
                                 <td>{{ $report->crack_length }}</td>
