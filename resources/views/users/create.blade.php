7e784fe4052ea40fcc19429e9156e7c9b0a16c65
Cycleurope <54021538+Cycleurope@users.noreply.github.com>
2020-06-24 16:49:44 +0200

    Update create.blade.php

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -4,6 +4,26 @@
     <div class="row">
         <div class="col-12">
             <h1>New user</h1>
+            <form action="">
+                @csrf
+                <div class="form-group">
+                    <label for="login">Identifiant / Login</label>
+                    <input type="text" name="login" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label for="password">Mot de passe</label>
+                    <input type="text" name="password" id="password">
+                </div>
+                <div class="form-group">
+                    <label for="name">Nom</label>
+                    <input type="text" name="name" id="name">
+                </div>
+                <div class="form-group">
+                    <label for="email">Adresse e-mail</label>
+                    <input type="email" name="email" id='email'>
+                </div>
+                <input type="submit" value="Valilider" class="btn btn-block btn-primary">
+            </form>
         </div>
     </div>
 </div>
