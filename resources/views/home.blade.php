6460de84c5758c7128dd5499d76c4a6d4cbc404c
Cycleurope <54021538+Cycleurope@users.noreply.github.com>
2020-06-30 09:45:15 +0200

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,6 @@
     <div class="row justify-content-center">
         <div class="col-md-12">
             <h1>Bienvenue</h1>
-            <h2>sur le service en ligne<br />de rapports de cadres La Poste</h2>
         </div>
     </div>
 </div>
