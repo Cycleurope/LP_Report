8c4ebf5205cfdd6798d561fc48b31de1d8089711
Cycleurope <54021538+Cycleurope@users.noreply.github.com>
2020-07-30 14:58:44 +0200

    Update import.blade.php

--- resources/views/reports/import.blade.php
+++ resources/views/reports/import.blade.php
@@ -24,7 +24,7 @@
                                     </div>
                                     <div class="form-group col-8">
                                         <label for="serial">Numéro de châssis (8 chiffres)</label>
-                                        <input type="text" name="serial" id="serial" class="form-control" maxchars="8" placeholder="" style="text-align: left;">
+                                        <input type="text" name="serial" id="serial" class="form-control" maxchars="8" placeholder="1 6 _ _ _ _ _ _" style="text-align: left;">
                                     </div>
                                     <div class="col-12">
                                         <h4>Bureau de Poste</h4>
